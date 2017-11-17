6052d5bc0e8c1f66c11393fd97518c0d35fcf481
Rimvydas Naktinis <naktinis@gmail.com>
2017-11-17 16:18:55 +0100

    VoxForge dataset generator takes arguments

--- voxforge_set.sh
+++ voxforge_set.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "Provide input and output csv files."
+    exit
+fi
+
 # Use 1 archive per speaker
 # Take 5 samples per archive
 # Take 500 samples per language
 per_lang=500
-filename='voxforge_set1.csv'
-rm -f $filename
-grep "\(,1\|,2\|,3\|,4\|,5\)$" voxforge_samples.log | grep German | awk -F, '{print $1","$2}' | head -n $per_lang >> $filename
-grep "\(,1\|,2\|,3\|,4\|,5\)$" voxforge_samples.log | grep English | awk -F, '{print $1","$2}' | head -n $per_lang >> $filename
-grep "\(,1\|,2\|,3\|,4\|,5\)$" voxforge_samples.log | grep Spanish | awk -F, '{print $1","$2}' | head -n $per_lang >> $filename
-grep "\(,1\|,2\|,3\|,4\|,5\)$" voxforge_samples.log | grep Italian | awk -F, '{print $1","$2}' | head -n $per_lang >> $filename
-grep "\(,1\|,2\|,3\|,4\|,5\)$" voxforge_samples.log | grep Portuguese | awk -F, '{print $1","$2}' | head -n $per_lang >> $filename
-shuf $filename -o $filename
-wc -l $filename
-head $filename
+in_file=$1
+out_file=$2
+rm -f $out_file
+grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep German | awk -F, '{print $1","$2}' | head -n $per_lang >> $out_file
+grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep English | awk -F, '{print $1","$2}' | head -n $per_lang >> $out_file
+grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep Spanish | awk -F, '{print $1","$2}' | head -n $per_lang >> $out_file
+grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep Italian | awk -F, '{print $1","$2}' | head -n $per_lang >> $out_file
+grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep Portuguese | awk -F, '{print $1","$2}' | head -n $per_lang >> $out_file
+shuf $out_file -o $out_file
+wc -l $out_file
+head $out_file
