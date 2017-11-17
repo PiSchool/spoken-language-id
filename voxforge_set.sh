ebcc9cabdbef1123be79e9f21c43d1bdba9ecdc9
Rimvydas Naktinis <naktinis@gmail.com>
2017-11-17 18:52:17 +0100

    VoxForge dataset generator groups users together
    However, it keeps languages shuffled. This makes it easy to split the
    dataset into training and evaluation subsets in a way that does not
    reuse the same user recordings in the two subsets.

--- voxforge_set.sh
+++ voxforge_set.sh
@@ -8,7 +8,7 @@ fi
 # Use 1 archive per speaker
 # Take 5 samples per archive
 # Take 500 samples per language
-per_lang=500
+per_lang=550
 in_file=$1
 out_file=$2
 rm -f $out_file
@@ -17,5 +17,9 @@ grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep English | awk -F, '{print $1","$2
 grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep Spanish | awk -F, '{print $1","$2}' | head -n $per_lang >> $out_file
 grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep Italian | awk -F, '{print $1","$2}' | head -n $per_lang >> $out_file
 grep "\(,1\|,2\|,3\|,4\|,5\)$" $in_file | grep Portuguese | awk -F, '{print $1","$2}' | head -n $per_lang >> $out_file
+
+# Shuffled so that user files are grouped together, but languages are mixed
+sort -R -k 1.1,1.5 $out_file -o $out_file
+
 wc -l $out_file
 head $out_file
