f3f6c747827afae19d9ec2f854df66fb46c5d409
Rimvydas Naktinis <naktinis@gmail.com>
2017-11-24 10:10:46 +0100

    Moved convolution and max-pooling layers into a separate function

--- models/combo.py
+++ models/combo.py
@@ -1,6 +1,16 @@
 import tensorflow as tf
 
 
+def convpool_layers(inputs, training, filters, kernel_size, pool_size, strides, normalize, pool_dropout):
+    conv = tf.layers.conv2d(inputs, filters=filters, kernel_size=kernel_size, activation=tf.nn.relu)
+    if pool_dropout:
+        conv = tf.layers.dropout(conv, rate=pool_dropout, training=training)
+    pool = tf.layers.max_pooling2d(conv, pool_size=pool_size, strides=strides, padding='same')
+    if normalize:
+        pool = tf.contrib.layers.layer_norm(pool)
+    return pool
+
+
 def languid_combo(features, training, params):
     batch_size = tf.shape(features)[0]
     input_sgram = tf.reshape(features, [batch_size, -1, params.spectrogram_bins])
@@ -12,36 +22,26 @@ def languid_combo(features, training, params):
     input_reshaped = tf.expand_dims(input_reshaped, -1)
 
     with tf.variable_scope("CNN"):
-        # TODO move CNN layer creation to a method
-        conv1 = tf.layers.conv2d(input_reshaped, filters=16, kernel_size=7, activation=tf.nn.relu)
-        pool1 = tf.layers.max_pooling2d(conv1, pool_size=3, strides=(2, 1), padding='same')
-        if params.normalize:
-            pool1 = tf.contrib.layers.layer_norm(pool1)
-
-        conv2 = tf.layers.conv2d(pool1, filters=32, kernel_size=5, activation=tf.nn.relu)
-        if params.pool_dropout:
-            conv2 = tf.layers.dropout(conv2, rate=params.pool_dropout, training=training)
-        pool2 = tf.layers.max_pooling2d(conv2, pool_size=3, strides=(2, 1), padding='same')
-        if params.normalize:
-            pool2 = tf.contrib.layers.layer_norm(pool2)
-
-        conv3 = tf.layers.conv2d(pool2, filters=32, kernel_size=3, activation=tf.nn.relu)
-        if params.pool_dropout:
-            conv3 = tf.layers.dropout(conv3, rate=params.pool_dropout, training=training)
-        pool3 = tf.layers.max_pooling2d(conv3, pool_size=3, strides=(2, 1), padding='same')
-        if params.normalize:
-            pool3 = tf.contrib.layers.layer_norm(pool3)
-
-        conv4 = tf.layers.conv2d(pool3, filters=32, kernel_size=3, activation=tf.nn.relu)
-        if params.pool_dropout:
-            conv4 = tf.layers.dropout(conv4, rate=params.pool_dropout, training=training)
-        pool4 = tf.layers.max_pooling2d(conv4, pool_size=3, strides=(2, 1), padding='same')
-        if params.normalize:
-            pool4 = tf.contrib.layers.layer_norm(pool4)
+        convpool = convpool_layers(
+            input_reshaped, training, filters=16, kernel_size=7, pool_size=3, strides=(2, 1),
+            normalize=params.normalize, pool_dropout=params.pool_dropout,
+        )
+        convpool = convpool_layers(
+            convpool, training, filters=32, kernel_size=5, pool_size=3, strides=(2, 1),
+            normalize=params.normalize, pool_dropout=params.pool_dropout,
+        )
+        convpool = convpool_layers(
+            convpool, training, filters=32, kernel_size=3, pool_size=3, strides=(2, 1),
+            normalize=params.normalize, pool_dropout=params.pool_dropout,
+        )
+        convpool = convpool_layers(
+            convpool, training, filters=32, kernel_size=3, pool_size=3, strides=(2, 1),
+            normalize=params.normalize, pool_dropout=params.pool_dropout,
+        )
 
     # The shape was (batch, bins, timesteps, filters), and should become (batch, timesteps, features)
     # TODO because of difference in pooling I get (32, 6, 844, 32) and orig. has (32, 8, 852, 32)
-    input_gru = tf.transpose(pool4, perm=[0, 2, 3, 1])
+    input_gru = tf.transpose(convpool, perm=[0, 2, 3, 1])
     input_shape = input_gru.get_shape()
     input_gru = tf.reshape(input_gru, tf.stack([batch_size, -1, input_shape[2] * input_shape[3]]))
 
